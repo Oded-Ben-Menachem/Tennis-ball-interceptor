db5f6ad837a10c316d3393fae4972550506b75c4
oded-ben-menachem <odedbenmen@gmail.com>
2026-03-15 11:34:58 +0200

    order before finish

--- install try.py
+++
@@ -1,8 +0,0 @@
-import sys
-import subprocess
-
-# פקודה להתקנה ישירות לתוך ה-Interpreter הנוכחי
-subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
-
-import cv2
-print("הצלחה! גרסת OpenCV היא:", cv2.__version__)
